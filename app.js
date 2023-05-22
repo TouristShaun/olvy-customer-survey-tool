501232015e89adbc5b15530544a11157952944d6
ChatGPT <chatgpt@openai.com>
2023-05-22 18:05:42 -0400

    Update app.js to include survey routes and view engine

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+var surveyRoutes = require('./routes/survey');
+app.use('/survey', expressStatic('./public'));
+app.use('/survey', surveyRoutes);
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
